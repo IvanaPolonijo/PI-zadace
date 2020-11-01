74ea5596b506238955bc80833af3bd85a315d2ff
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-01 19:15:23 +0100

    Create JS-301.js

---
+++ JS-301.js
@@ -0,0 +1,11 @@
+// Napisati funkciju min koja prima dva argumenta i vraća natrag manjeg.
+let a;
+let b;
+
+function min(a, b){
+    if (a < b)
+        return a;
+    else return b;
+    }
+
+console.log(min(4, -7))
