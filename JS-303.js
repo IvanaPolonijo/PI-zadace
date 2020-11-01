8fdfd0d1f0987fda05443486b6f231a5d054a22d
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-01 21:10:12 +0100

    JS-303 Ackermann

---
+++ JS-303.js
@@ -0,0 +1,20 @@
+// Implementirati Ackermann-ovu funkciju:
+// https://gyazo.com/13d2277c32790990b50d219f8d911033
+var m = 3;
+var n = 2;
+
+function ackermann (m,n){
+    if (m == 0){ 
+        return n + 1; 
+    } 
+    else if((m > 0) && (n == 0)){ 
+        return ackermann(m - 1, 1); 
+    } 
+    else if((m > 0) && (n > 0)){ 
+        return ackermann(m - 1, ackermann(m, n - 1)); 
+    } 
+    else
+        return ("Obje varijable moraju biti pozitivne!")
+    } 
+
+console.log(ackermann(m, n)) 
