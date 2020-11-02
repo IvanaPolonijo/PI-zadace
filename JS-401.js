655bda8b978e7fea190d61139bbb1b2ccbf3cf3f
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-02 19:16:28 +0100

    testirano

--- JS-401.js
+++ JS-401.js
@@ -3,58 +3,69 @@
 // provjeri(lista, pojam) koja vraća true ukoliko postoji student na lista čije ime ili
 // prezime je baš pojam , a upisan je.
 
-let imeprezime = 'Slavica';
+let imeprezime = 'Marko';
 
 let student = [
-    {ime: 'Ivana' , 
-    prezime: 'Polonijo',
-    upisan: true,
+    {
+        ime: 'Ivana',
+        prezime: 'Polonijo',
+        upisan: true,
     },
-    {ime: 'Ivana' , 
-    prezime: 'Devlic',
-    upisan: true,
+    {
+        ime: 'Ivana',
+        prezime: 'Devlic',
+        upisan: true,
     },
-    {ime: 'Ivana' , 
-    prezime: 'Polonijo',
-    upisan: false,
-    },    
-    {ime: 'Martina' , 
-    prezime: 'Martinovic',
-    upisan: true,
+    {
+        ime: 'Ivana',
+        prezime: 'Polonijo',
+        upisan: false,
     },
-    {ime: 'Ana' , 
-    prezime: 'Kralj',
-    upisan: false,
+    {
+        ime: 'Martina',
+        prezime: 'Martinovic',
+        upisan: true,
     },
-    {ime: 'Ana' , 
-    prezime: 'Mart',
-    upisan: false,
+    {
+        ime: 'Ana',
+        prezime: 'Kralj',
+        upisan: false,
     },
-    {ime: 'Lana' , 
-    prezime: 'Kralj',
-    upisan: true,
+    {
+        ime: 'Ana',
+        prezime: 'Mart',
+        upisan: false,
     },
-    {ime: 'Leda' , 
-    prezime: 'Frleta',
-    upisan: true,
+    {
+        ime: 'Lana',
+        prezime: 'Kralj',
+        upisan: true,
     },
-    {ime: 'Luka' , 
-    prezime: 'Tesla',
-    upisan: false,
+    {
+        ime: 'Leda',
+        prezime: 'Frleta',
+        upisan: true,
     },
-    {ime: 'Petra' , 
-    prezime: 'Devlic',
-    upisan: false,
+    {
+        ime: 'Luka',
+        prezime: 'Tesla',
+        upisan: false,
+    },
+    {
+        ime: 'Petra',
+        prezime: 'Devlic',
+        upisan: false,
     }
 ];
 
 
-function provjera (imeprezime){
-    for (let i=0; i<=(student.length-1); i++)
-    {   if (student[i].upisan == true && (student[i].ime == imeprezime || student[i].prezime == imeprezime)) 
+function provjera(imeprezime) {
+    for (let i = 0; i <= (student.length - 1); i++) //idemo od 0 indexsa do jednog manje od ukupne dužine arraya 
+    {
+        if (student[i].upisan == true && (student[i].ime == imeprezime || student[i].prezime == imeprezime))
             return true;
-        }
-    return false;    
-}    
+    }
+    return false;
+}
 
 console.log(provjera(imeprezime));
--- JS-402.js
+++ JS-402.js
@@ -8,55 +8,67 @@ let imeprezime = "ivana";
 let status = false;
 
 let student = [
-    {ime: 'Ivana' , 
-    prezime: 'Polonijo',
-    upisan: true,
+    {
+        ime: 'Ivana',
+        prezime: 'Polonijo',
+        upisan: true,
     },
-    {ime: 'Ivana' , 
-    prezime: 'Devlic',
-    upisan: true,
+    {
+        ime: 'Ivana',
+        prezime: 'Devlic',
+        upisan: true,
     },
-    {ime: 'Ivana' , 
-    prezime: 'Polonijo',
-    upisan: false,
-    },    
-    {ime: 'Martina' , 
-    prezime: 'Martinovic',
-    upisan: true,
+    {
+        ime: 'Ivana',
+        prezime: 'Polonijo',
+        upisan: false,
     },
-    {ime: 'Ana' , 
-    prezime: 'Kralj',
-    upisan: false,
+    {
+        ime: 'Martina',
+        prezime: 'Martinovic',
+        upisan: true,
     },
-    {ime: 'Ana' , 
-    prezime: 'Mart',
-    upisan: false,
+    {
+        ime: 'Ana',
+        prezime: 'Kralj',
+        upisan: false,
     },
-    {ime: 'Lana' , 
-    prezime: 'Kralj',
-    upisan: true,
+    {
+        ime: 'Ana',
+        prezime: 'Mart',
+        upisan: false,
     },
-    {ime: 'Leda' , 
-    prezime: 'Frleta',
-    upisan: true,
+    {
+        ime: 'Lana',
+        prezime: 'Kralj',
+        upisan: true,
     },
-    {ime: 'Luka' , 
-    prezime: 'Tesla',
-    upisan: false,
+    {
+        ime: 'Leda',
+        prezime: 'Frleta',
+        upisan: true,
     },
-    {ime: 'Petra' , 
-    prezime: 'Devlic',
-    upisan: false,
+    {
+        ime: 'Luka',
+        prezime: 'Tesla',
+        upisan: false,
+    },
+    {
+        ime: 'Petra',
+        prezime: 'Devlic',
+        upisan: false,
     }
 ];
 
-function provjera (imeprezime){
-    for (let i=0; i<=(student.length-1); i++)
-    {   if (student[i].upisan == status && 
-        (student[i].ime.toLowerCase() == imeprezime.toLowerCase() || student[i].prezime.toLowerCase() == imeprezime.toLowerCase())) 
+function provjera(imeprezime) {
+    for (let i = 0; i <= (student.length - 1); i++) //idemo od 0 indexsa do jednog manje od ukupne dužine arraya 
+    {
+        if (student[i].upisan == status &&
+            (student[i].ime.toLowerCase() == imeprezime.toLowerCase() || student[i].prezime.toLowerCase() == imeprezime.toLowerCase()))
+            //ne znam jel može ovaj if da ljepše izgleda :)
             return console.log("Postoji!");
-        }
-    return console.log("Ne postoji!");    
-}    
+    }
+    return console.log("Ne postoji!");
+}
 
-provjera (imeprezime);
+provjera(imeprezime);
