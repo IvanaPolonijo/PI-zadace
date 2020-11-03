055b5d4ef9363d03943b43cc21297666559fc811
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-03 10:34:21 +0100

    JS-403 testiran

---
+++ JS-403.js
@@ -0,0 +1,29 @@
+// Napiši funkciju zagrade koja će provjeriti jesu li zagrade valjano ugnježdene:
+
+let map = { //hash uređenih parova
+    '(': ')',
+    '[': ']',
+    '{': '}'
+}
+
+let zagrade = function (zagrade) {
+    let lijevi = [];
+    for (i=0; i < zagrade.length; i++){
+        if(zagrade[i] == "{" || zagrade[i] == "[" || zagrade[i] == "(")
+        {
+            lijevi.push(zagrade[i]); //ako je lijeva zagrada guram je na pomoćni stog
+        }
+        else { 
+            let temp = lijevi.pop(); //ako zagrada je desna skidam zadnji sa lijevog stoga 
+            if(zagrade[i] !== map[temp]) // i uspoređujem s uređenim parovima hash tablice 
+            {return false}; //ako si ne odgovaraju prekidam funkciju i vraćam false...
+        }
+    }    
+    if (lijevi.length !== 0) //provjera da nije još nešto ustalo u stogu od lijevih zagrada
+    {return false};    
+return true; //vracam true ako je prošlo sve provjere
+}
+  
+console.log(zagrade("([()]()()"));
+console.log(zagrade("({)}")); 
+console.log(zagrade("{[((()))]}"));
