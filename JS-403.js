17ca0d97ff9ca390ead48860ad1ef1a27b23d5c7
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-03 10:40:01 +0100

    testiran

--- JS-403.js
+++ JS-403.js
@@ -8,22 +8,21 @@ let map = { //hash uređenih parova
 
 let zagrade = function (zagrade) {
     let lijevi = [];
-    for (i=0; i < zagrade.length; i++){
-        if(zagrade[i] == "{" || zagrade[i] == "[" || zagrade[i] == "(")
-        {
+    for (i = 0; i < zagrade.length; i++) {
+        if (zagrade[i] == "{" || zagrade[i] == "[" || zagrade[i] == "(") {
             lijevi.push(zagrade[i]); //ako je lijeva zagrada guram je na pomoćni stog
         }
-        else { 
+        else {
             let temp = lijevi.pop(); //ako zagrada je desna skidam zadnji sa lijevog stoga 
-            if(zagrade[i] !== map[temp]) // i uspoređujem s uređenim parovima hash tablice 
-            {return false}; //ako si ne odgovaraju prekidam funkciju i vraćam false...
+            if (zagrade[i] !== map[temp]) // i uspoređujem s uređenim parovima hash tablice 
+            { return false }; //ako si ne odgovaraju prekidam funkciju i vraćam false...
         }
-    }    
+    }
     if (lijevi.length !== 0) //provjera da nije još nešto ustalo u stogu od lijevih zagrada
-    {return false};    
-return true; //vracam true ako je prošlo sve provjere
+    { return false };
+    return true; //vracam true ako je prošlo sve provjere
 }
-  
+
 console.log(zagrade("([()]()()"));
-console.log(zagrade("({)}")); 
+console.log(zagrade("({)}"));
 console.log(zagrade("{[((()))]}"));
