8777a69528f8b2b87553ed5b248cfa90afbc2e34
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-10 16:06:28 +0100

    Update vjezba1_zadatak2.js

--- vjezba1_zadatak2.js
+++ vjezba1_zadatak2.js
@@ -7,7 +7,7 @@ INPUT.forEach(ponavljanje);
 function ponavljanje(INPUT) {
     for (i = 0; i < (INPUT.length - 1); i++) { //vrti for od nule do -1 dužine stringa jer array index kreće od 0
         if (INPUT[i] === INPUT[i + 1])
-            INPUT = INPUT.replace(INPUT[i+1], "")                      // ako su dva susjedna brisemo prvi  
+            INPUT = INPUT.replace(INPUT[i], "")                      // ako su dva susjedna brisemo prvog
     }
     return console.log(INPUT);
 }
