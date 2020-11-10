0b3e867121b7e17854a96b62297a63a62a6b0047
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-10 15:38:50 +0100

    Update vjezba1_zadatak1.js

--- vjezba1_zadatak1.js
+++ vjezba1_zadatak1.js
@@ -1,6 +1,6 @@
 INPUT = {
     "prvi": 122,
-    "drugi": 18.4,
+    "drugi": 18.4,//dodala decimalni da testiram 
     "treci": "NotAnInteger",
     "cetvrti": -2,
     "peti": 322,
@@ -9,9 +9,9 @@ INPUT = {
 
 function f(INPUT) {
     let total = 0;
-    for (const i in INPUT) 
+    for (const i in INPUT) //provjeri svakog 
     {
-       if (Number.isInteger(INPUT[i]))
+       if (Number.isInteger(INPUT[i])) //provjera je li cijeli broj 
                 total += INPUT[i];
     }
     return total;
