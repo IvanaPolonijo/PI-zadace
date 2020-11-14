31c1dca52ab492ce2334ab68b1b784a9152ff1d5
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-15 00:19:26 +0100

    testiranje u tijeku

---
+++ vj2_z1.js
@@ -0,0 +1,32 @@
+// 1. Implementirajte funkciju isDivisible koja provjerava je li broj dijeljiv s brojem tri i vraća
+// boolean vrijednost. Ograničenja su sljedeća: zabranjeno je korištenje operatora "/" i "%".
+// Input ne smije prazan, ne smije sadržavati ništa osim cijelih brojeva i naravno 0.
+var unos = "6.2";
+
+function isDivisible(unos) {
+    if (isNaN(unos) == false) {
+        unos = Number(unos);
+        if (unos === 0) return console.log ("0 nije nikad djeljiva");
+        else if(!Number.isInteger(unos)) console.log ("Nije cijeli broj")
+        else {
+            let n = 0;
+            zbrajanje(n);
+        }
+    }
+    else return console.log ("Nije broj");
+}
+
+function zbrajanje (n){
+    n+=3;
+    if (n > unos) return (console.log ("nije djeljiv"));
+    else if (n == unos) return (console.log ("jest djeljiv"));
+    else zbrajanje (n);
+}
+
+isDivisible(unos)
+// console.log(isDivisible("9")) //True
+// console.log(isDivisible("333")) //True
+// console.log(isDivisible("334")) //False
+// console.log(isDivisible("0")) //Input nije valjan
+// console.log(isDivisible("a")) //Input nije valjan
+// console.log(isDivisible("")) //?
