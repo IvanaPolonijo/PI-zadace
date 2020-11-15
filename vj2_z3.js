fbed936b8ec915272b003b5655632b1ca973db9a
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-15 23:55:48 +0100

    u radu

---
+++ vj2_z3.js
@@ -0,0 +1,17 @@
+// 3. Implementirajte funkciju reverseEverything koja kao input prima rečenicu, odnosno string,
+// a vraća zamijenjen redoslijed svake riječi i slova u rečenici. Input ne smije biti prazan i
+// rečenicu moraju činiti makar dvije riječi. U outputu riječi moraju biti odvojene jednim i samo
+// jednim praznim mjestom.
+
+let obrnuti;  //stack gdje mogu ubacivati oburnuto
+
+function reverseEverything(string) {
+    {for (let i = 0; i < string.length; i++)
+        console.log(string.splice)     
+    };
+    return obrnuti;
+}
+
+
+console.log(reverseEverything("Riba ribi grize rep")) //"per ezirg ibir abiR"
+console.log(reverseEverything("Riba_ribi_grize_rep")) //Input nije valjan
