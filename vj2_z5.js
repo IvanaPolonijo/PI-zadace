7bd7308f6596a10e74e67152dfda376a67fa5abb
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-16 22:53:40 +0100

    Update vj2_z5.js
    regexp za duljinu mi ne radi kako spada u alertima

--- vj2_z5.js
+++ vj2_z5.js
@@ -4,27 +4,28 @@
 // ispišite zašto input ne zadovoljava pravilo. Istu funkciju možete iskoristiti za svoj projekt.
 
 function passwordValidator(string) {
-    let pass = /(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,15})/
+    let pass = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?!.*\s).{6,15}$/
 
     //regexp mozda iznjeti van funkije kao var da se koristi i na drugim  funkcijama?
     if (string.match(pass)) return true;
     else {
         if (!string.match(/(?=.*[A-Z])/))
             console.log("Nedostaje veliko slovo")
-        else if (!string.match(/(?=.*[0-9])/))
+        if (!string.match(/(?=.*[0-9])/))
             console.log("Nedostaje znamenka")
-        else if (!string.match(/(?=.*[!@#\$%\^&\*])/))
+        if (!string.match(/(?=.*[!@#\$%\^&\*])/))
             console.log("Nedostaje poseban znak")
-        else console.log("Nije odgovarajuce duljine!")
+        if (!string.match(/(?!.*\s).{6,15}/))  //ovaj mi ne vraca dobro
+            console.log("Nije odgovarajuce duljine!")
         return false;
     };
 
 }
 
-// vraca mi prvi problem na koji naleti a ne sve ...
+// je li okay vracanje tog if toliko puta da mi ispise svaki error  
 // RegExp(^  (?=.*[A-Z])  (?=.*[0-9]) (?=.*[!@#\$%\^&\*])  (?=.{6,15}) )
 
 console.log(passwordValidator("123Aa!")) //True
 console.log(passwordValidator("A")) //False -> Input je prekratak
 console.log(passwordValidator("123aa!")) //False -> Nedostaje veliko slovo 
-console.log(passwordValidator("123aa!fzseivsrhanz7654"))
+console.log(passwordValidator("123aa#fzseivsrhanz7654asfefA"))
