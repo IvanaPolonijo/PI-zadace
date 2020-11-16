d3175182176f495739cde9d1f5da41b3e95e726c
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-16 16:58:43 +0100

    u radu

--- vj2_z3.js
+++ vj2_z3.js
@@ -7,8 +7,9 @@ function reverseEverything(string) {
     let pomocni = string.split('');  //string u stack da mogu čupati obrnuto slova
     var check = 0;
     let obrnutiStr = "";
-    for (let i =(string.length -1); i >= 0; i--) {
-        if (pomocni[i] === " ") {check++} //markiram ako se pojavilo prazno mjesto 
+    for (let i = (string.length - 1); i >= 0; i--) {
+        if (pomocni[i] === " ") { check++ } //markiram ako se pojavilo prazno mjesto. zanimljivo! radi samo ako
+        //fakat imam prazno mjesto između navodnika
         obrnutiStr += pomocni.pop(); //slozim obrnuti string iz stacka
     }
     if (check == 0) return "Input nije valjan."
@@ -20,4 +21,4 @@ console.log(reverseEverything("Riba ribi grize rep")) //"per ezirg ibir abiR"
 console.log(reverseEverything("Riba_ribi_grize_rep")) //Input nije valjan
 
 //razmisljam o sceanariju da je blank na pocetku rijecu ili kraju
-//treba jos pokreti scenarij da je samo jedna rijec
+//treba jos pokriti scenarij da je samo jedna rijec
